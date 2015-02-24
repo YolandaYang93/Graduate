6457100834819d6a2a714c374202fdfa918de693
yinchuandong <yincd520@sina.com>
2015-02-24 14:19:36 +0800

    compelte header

--- ClassSignin/src/com/example/classsignin/widget/CurriculumView.java
+++ ClassSignin/src/com/example/classsignin/widget/CurriculumView.java
@@ -8,6 +8,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PaintFlagsDrawFilter;
 import android.graphics.Typeface;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -47,7 +48,7 @@ public class CurriculumView extends RelativeLayout{
 	
 	private void init(){
 		initView();
-		initData();
+//		initData();
 	}
 	
 
@@ -64,8 +65,8 @@ public class CurriculumView extends RelativeLayout{
             buts[num].setBackground(getResources().getDrawable(R.drawable.shape_curriculum));
             oParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
             oParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-            oParams.leftMargin = 100 * num;
-            oParams.topMargin = 60;
+            oParams.leftMargin = 100 * (num + 1);
+            oParams.topMargin = 100;
             
             this.addView(buts[num],oParams);
         }
@@ -73,8 +74,8 @@ public class CurriculumView extends RelativeLayout{
 	
 	private void initData(){
 		courseNum = 13;
-		this.headerGridWidth = (int)(getWidth() * 0.1);
-		this.headerGridHeight = (int)(getHeight() * 0.05);
+		this.headerGridWidth = (int)(getWidth() * 0.08);
+		this.headerGridHeight = (int)(getHeight() * 0.08);
 		this.gridWidth = (getWidth() - headerGridWidth) / headerRow.length;
 		this.gridHeight = (getHeight() - headerGridHeight) / courseNum;
 	}
@@ -84,19 +85,35 @@ public class CurriculumView extends RelativeLayout{
 		super.onDraw(canvas);
 		initData();
 		
+//		canvas.setDrawFilter(new PaintFlagsDrawFilter(0, Paint.ANTI_ALIAS_FLAG|Paint.FILTER_BITMAP_FLAG)); 
 		Paint headerPaint = new Paint();
 		headerPaint.setAlpha(50);
 		canvas.drawRect(0, 0, getWidth(), headerGridHeight, headerPaint);
+		canvas.drawRect(0, headerGridHeight, headerGridWidth, getHeight(), headerPaint);
 		
 		Paint textPaint = new Paint();
 		textPaint.setColor(Color.WHITE);
 		textPaint.setTypeface(Typeface.create("微软雅黑", Typeface.BOLD));
+		
+		Paint sepPaint = new Paint();
+		sepPaint.setColor(Color.GRAY);
 		for (int i = 0; i < headerRow.length; i++) {
+			//draw number
 			textPaint.setTextSize(16);
-			canvas.drawText(String.valueOf(i + 1), headerGridWidth + gridWidth * i + gridWidth / 3, headerGridHeight / 2 - 10, textPaint);
+			canvas.drawText(String.valueOf(i + 1), headerGridWidth + gridWidth * i + gridWidth / 2 - 5, headerGridHeight / 2 - 10, textPaint);
+			//draw weekday
 			textPaint.setTextSize(14);
-			canvas.drawText(headerRow[i], headerGridWidth + gridWidth * i, headerGridHeight / 2 + 15, textPaint);
+			canvas.drawText(headerRow[i], headerGridWidth + gridWidth * i + gridWidth / 2 - 20, headerGridHeight / 2 + 15, textPaint);
+			//draw seprator
+			canvas.drawLine(headerGridWidth + gridWidth * i, 0, headerGridWidth + gridWidth * i, headerGridHeight - 1, sepPaint);
 		}
+		
+		for (int i = 0; i < this.courseNum; i++) {
+			canvas.drawText(String.valueOf(i+1), headerGridWidth / 2 - 5, headerGridHeight + gridHeight * i + gridHeight / 2, textPaint);
+			canvas.drawLine(0, headerGridHeight + gridHeight * i, headerGridWidth - 1, headerGridHeight + gridHeight * i, sepPaint);
+		}
+		
+		
 	}
 	
 	
